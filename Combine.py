bc978a7ddc1a2420e461365c3bf3c40727344bde
eccelaine <47830922+eccelaine@users.noreply.github.com>
2021-03-01 12:12:34 -0600

    Update Combine.py

--- Combine.py
+++ Combine.py
@@ -1,7 +1,8 @@
 import csv
 
 FILE1="clustered-heavies.dat"
-FILE2="clustered-lights.dat"
+
+PAIRED='paired.csv'
 
 MISSING="missing.dat"
 mout = open(MISSING,'w')
@@ -29,41 +30,71 @@ with open(FILE1) as fin:
             hout_hash[ls[0]].append(hc_id)
             
 cluster_index = 0
-with open(FILE2) as fin:
-    for line in fin:
-        if "END" in line:
-            cluster_index += 1
-            continue
+# with open(FILE2) as fin:
+#     for line in fin:
+#         if "END" in line:
+#             cluster_index += 1
+#             continue
         
-        ls = line.strip().split(' ')
-        lc_id = ls[3]+'_'+ls[4]+'_'+str(len(ls[5]))+'_'+str(cluster_index)
+#         ls = line.strip().split(' ')
+#         lc_id = ls[3]+'_'+ls[4]+'_'+str(len(ls[5]))+'_'+str(cluster_index)
+        
+#         d = None
+#         if ls[0] not in hout_hash:
+#             mout.write(ls[0] + '\n')
+#             continue
+            
+#         for h in hout_hash[ls[0]]:
+#             if ls[0] in hout[h]:
+#                 d = hout[h]
+#                 break
+            
+#         if not d:
+#             print("Missing:", ls[0])
+#         else:
+#             d[ls[0]]['lights'].append({'cluster': lc_id, 'ls': ls})
+
+light_clusters = {}
+with open(PAIRED) as fin:
+    fin.readline()
+    for line in fin:
+#        if "END" in line:
+#            cluster_index += 1
+#            continue
+#        
+        ls = line.strip().split(',')
+        clonotype_id = ls[0] + '_' + ls[1] + '_' + ls[3]
+        cluster_id = ls[19] + '_' + ls[20]
+        #lc_id = ls[0]+'_'+ls[1]+'_'+
         
         d = None
-        if ls[0] not in hout_hash:
-            mout.write(ls[0] + '\n')
+        if clonotype_id not in hout_hash:
+            mout.write(clonotype_id + '\n')
             continue
             
-        for h in hout_hash[ls[0]]:
-            if ls[0] in hout[h]:
+        for h in hout_hash[clonotype_id]:
+            if clonotype_id in hout[h]:
                 d = hout[h]
                 break
             
         if not d:
-            print("Missing:", ls[0])
+            print("Missing:", clonotype_id)
         else:
-            d[ls[0]]['lights'].append({'cluster': lc_id, 'ls': ls})
+            d[clonotype_id]['lights'].append({'cluster_id': cluster_id, 'ls': ls})
+
+            
 
 i = 0
 for key in hout:
     if i > 10:
         break
         
-    print(hout[key])
+    #print(hout[key])
     i += 1
     
-print(len(hout))
+#print(len(hout))
 mout.close()
-print(hout.keys())
+#print(hout.keys())
 
 with open('10Xout.csv', 'w') as fout:
     fout.write('function,mongo_id,heavy_id,heavy_v,heavy_j,heavy_cdr3,heavy_cluster,light_id,light_v,light_j,light_cdr3,light_cluster,heavy_raw,light_raw\n')
@@ -76,4 +107,5 @@ with open('10Xout.csv', 'w') as fout:
 #             print(d)
             hls = d['heavies'][0]['ls']
             lls = d['lights'][0]['ls']
-            fout.write(hls[2] +','+ hls[0] + ',' + hls[1] + ',' + hls[3] + ',' + hls[4] + ',' + hls[5] + ',' + cluster_id + ',' + lls[1] + ',' + lls[3] + ',' + lls[4] + ',' + lls[5] + ',' + d['lights'][0]['cluster'] + ',' + hls[7] + ',' + lls[7] + '\n')
+            #print(lls)
+            fout.write(hls[2] +','+ hls[0] + ',' + hls[1] + ',' + hls[3] + ',' + hls[4] + ',' + hls[5] + ',' + cluster_id + ',' + lls[3] + ',' + lls[19] + ',' + lls[20] + ',' + lls[22] + ',' + d['lights'][0]['cluster_id'] + ',' + hls[7] + ',' + lls[30] + '\n')
